d4d776aba562f0c7f73d849acba62b04bfb1af25
igoravelino-rgb <igor.avelino@escola.pr.gov.br>
2026-06-25 12:26:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const avanca = document.querySelectorAll('btn-proximo');
 
-avanca.forEach(button => ) {
+avanca.forEach(button =>{
     button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute()
-    })
-}
+        const atual = document.querySelector('.ativo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remover('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+     })
+})
+ 
